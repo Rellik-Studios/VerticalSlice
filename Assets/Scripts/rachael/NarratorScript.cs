916772e92347477f4ba3660f7559127434f879c7
himchawla <58020143+himchawla@users.noreply.github.com>
2021-10-26 02:21:12 +1300

    ink stuffff

--- Assets/Scripts/rachael/NarratorScript.cs
+++ Assets/Scripts/rachael/NarratorScript.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 
 public class NarratorScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static string m_date;
+
+    public static string m_time;
+
+    public static string m_userName;
+
+    public static string m_deviceName;
+
+    public static string m_weekDay;
     void Start()
     {
-        Debug.Log(System.DateTime.Now.TimeOfDay);
-        Debug.Log("The date is " + System.DateTime.Now.Day + "/" + System.DateTime.Now.Month + "/" + System.DateTime.Now.Year);
-        string theDate = System.DateTime.Now.ToString("hh:mm");
-        //Debug.Log("The time is" + System.DateTime.Now.Hour + ":" + System.DateTime.Now.Minute);
-        Debug.Log("The time is " + theDate);
+        m_userName = Environment.UserName;
+        m_deviceName = SystemInfo.deviceName;
 
-        string userName = Environment.UserName;
-
-        if(userName != null)
-            Debug.Log("You are defined as" + userName);
+        m_weekDay = System.DateTime.Now.DayOfWeek.ToString();
+        
+        if(m_userName != null)
+            Debug.Log("You are defined as" + m_userName);
         Debug.Log("Your device name is: " + SystemInfo.deviceName);
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_date = System.DateTime.Now.Day + "/" + System.DateTime.Now.Month + "/" + System.DateTime.Now.Year;
+        m_time = System.DateTime.Now.ToString("h:mm tt");
     }
 }
