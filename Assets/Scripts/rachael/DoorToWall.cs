21cb6976fccb488321ff5185c892c0b2be1ad5e4
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-10-26 16:54:26 +1300

    Door fade out and attempt at wall fade in

--- Assets/Scripts/rachael/DoorToWall.cs
+++ Assets/Scripts/rachael/DoorToWall.cs
@@ -6,6 +6,10 @@ public class DoorToWall : MonoBehaviour
 {
     [SerializeField] GameObject door;
     [SerializeField] GameObject wall;
+
+    //private bool fadeOut = false;
+    //public float fadeSpeed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +19,34 @@ public class DoorToWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    FadeOutObject();
+        //}
+        //if(fadeOut)
+        //{
+        //    Color outDoorColor = this.GetComponent<Renderer>().material.color;
+        //    //Color inDoorColor = GetComponentInChildren<Renderer>().material.color;
+        //    float fadeAmount = outDoorColor.a - (fadeSpeed * Time.deltaTime);
+
+        //    outDoorColor = new Color(outDoorColor.r, outDoorColor.g, outDoorColor.b, fadeAmount);
+        //   // inDoorColor = new Color(inDoorColor.r, inDoorColor.g, inDoorColor.b, fadeAmount);
+            
+
+        //    this.GetComponent<Renderer>().material.color = outDoorColor;
+        //    //GetComponentInChildren<Renderer>().material.color = inDoorColor;
+
+        //    if(outDoorColor.a <=0)
+        //    {
+        //        fadeOut = false;
+        //    }
+        //}
     }
+
+    //public void FadeOutObject()
+    //{
+    //    fadeOut = true;
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
