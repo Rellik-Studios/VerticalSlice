7ded0eff9e6a5fffd0b74a63445063126e05410f
himchawla <58020143+himchawla@users.noreply.github.com>
2021-10-29 17:24:09 +1300

    Narrator Updates

---
+++ Assets/Scripts/Himanshu/Narrator.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Himanshu
+{
+    public class Narrator : MonoBehaviour
+    {
+        
+        [SerializeField] private TMP_Text m_textBox;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_idleRoom;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_ballRoom;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_cafeteria;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_maze;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_bathroom;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_mirrorShatter;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hospital1950;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hospital1870;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hospitalFuturistic;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hospitalPresent;
+
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hub1;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_hub2;
+        
+        [TextArea(4, 6)]
+        [SerializeField] private List<string> m_finish1;
+
+        private bool m_settingText = false;
+        public void Play(List<string> _toPlay)
+        {
+            if (_toPlay.Count > 1 && !m_settingText)
+            {
+                int rand = Random.Range(1, _toPlay.Count);
+                StartCoroutine(SetText(_toPlay[rand], m_textBox));
+                _toPlay.RemoveAt(rand);
+            }
+            else if (!m_settingText)
+            {
+                StartCoroutine(SetText(_toPlay[0], m_textBox));
+            }
+        }
+
+        IEnumerator SetText(string _text, TMP_Text _textBox, bool additive = false)
+        {
+            m_settingText = true;
+            if(!additive) _textBox.text = "";
+            bool commandStart = false;
+            bool choiceStart = false;
+            bool choiceEnd = false;
+            string command = "";
+            int pos = 0;
+            List<string> choices = new List<string>();
+            string currentChoice = "";
+            foreach (var letter in _text)
+            {
+                pos++;
+                if (letter == '#')
+                {
+                    yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+                    StartCoroutine(SetText(_text.Substring(pos + 1), _textBox));
+                    yield break;
+                }
+
+                if (choiceStart)
+                {
+                    if (letter == '|')
+                    {
+                        choices.Add(currentChoice);
+                        currentChoice = "";
+                        continue;
+                    }
+                    else if (letter == '}')
+                    {
+                        choices.Add(currentChoice);
+                        choiceEnd = true;
+                        choiceStart = false;
+                    }
+                    else
+                    {
+                        currentChoice += letter;
+                        continue;
+                    }
+                }
+
+                if (choiceEnd)
+                {
+                    StartCoroutine(SetText(choices.Random(), _textBox, true));
+                    yield return new WaitWhile(() => m_settingText);
+                    m_settingText = true;
+                    choiceEnd = false;
+                    continue;
+                }
+
+                if (letter == '{')
+                {
+                    choiceStart = true;
+                    continue;
+                }
+                if (commandStart)
+                {
+                    if (letter == ' ' || letter == '?' || letter == ',')
+                    {
+                        StartCoroutine(SetText(EvaluateCommand(command), _textBox, true));
+                        yield return new WaitWhile(() => m_settingText);
+                        m_settingText = true;
+                        //_textBox.text += EvaluateCommand(command);
+                        commandStart = false;
+                    }
+                    else
+                    {
+                        command += letter;
+                        continue;
+                    }
+                    
+                }
+                if (letter == '@')
+                {
+                    commandStart = true;
+                    command = "";
+                }
+                else if (letter == ' ')
+                {
+                    _textBox.text += letter;
+                    yield return new WaitForSeconds(0.05f);
+                }
+                else
+                {
+                    _textBox.text += letter;
+                    yield return null;
+                }
+            }
+
+            m_settingText = false;
+        }
+
+        private string EvaluateCommand(string _command)
+        {
+            switch (_command)
+            {
+                case "userName":
+                    return NarratorScript.m_userName;
+                case "timeCategory":
+                    return NarratorScript.timeCategory;
+                case "day":
+                    return NarratorScript.m_weekDay;
+                case "time":
+                    return NarratorScript.m_time;
+                case "activity":
+                    return NarratorScript.activity;
+                default:
+                    return "Not Set Yet";
+            }
+            return "";
+        }
+
+        public void PlayHub()
+        {
+            Play(m_idleRoom);
+        }
+    }
+}
--- Assets/Scripts/rachael/NarratorScript.cs
+++ Assets/Scripts/rachael/NarratorScript.cs
@@ -14,17 +14,42 @@ public class NarratorScript : MonoBehaviour
     public static string m_deviceName;
 
     public static string m_weekDay;
+
+    public static string timeCategory
+    {
+        get
+        {
+            string cat;
+            int hour = DateTime.Now.Hour;
+            cat = hour >= 6 && hour < 12 ? "Morning" :
+                hour < 18 && hour >= 12 ? "Afternoon" :
+                hour >= 18 && hour < 21 ? "Evening" : "Night";
+           
+            return cat;
+        }
+    }
+
+    public static string activity
+    {
+        get
+        {
+            int hour = DateTime.Now.Hour;
+            return hour >= 5 && hour < 9 ? "You're up early, aren't you." :
+                hour < 17 && hour >= 9 ?   "Shouldn't you be working at this time? Or is 9 to 5 not your style?" :
+                hour >= 17 && hour < 20 ?  "Now's a good time to go outside, enjoy the daylight... why are you here?" :
+                hour >= 20 && hour < 22 ?  "This time in the evening is perfect for playing video games, I'd say." :
+                                           "It's awfully late. What's wrong, can't sleep?";
+        }
+    }
     void Start()
     {
         m_userName = Environment.UserName;
         m_deviceName = SystemInfo.deviceName;
-
         m_weekDay = System.DateTime.Now.DayOfWeek.ToString();
         
         if(m_userName != null)
             Debug.Log("You are defined as" + m_userName);
         Debug.Log("Your device name is: " + SystemInfo.deviceName);
-        
     }
 
     // Update is called once per frame
