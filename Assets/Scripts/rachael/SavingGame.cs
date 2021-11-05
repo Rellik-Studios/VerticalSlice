63fea8bcfe1c7a4800a0561e0da45545df8e3a27
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-11-06 11:06:10 +1300

    Update SavingGame.cs

--- Assets/Scripts/rachael/SavingGame.cs
+++ Assets/Scripts/rachael/SavingGame.cs
@@ -43,6 +43,11 @@ public class SavingGame : MonoBehaviour
             else
             {
                 LoadPoint();
+
+                //if(GetComponent<SavingGame>() != null)
+                //{
+                //    GetComponent<Player>().LoadPlayer();
+                //}
             }
         }
     }
